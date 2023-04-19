cf13cb724b1333386366406f5ab706df60716ba1
suryadheeshjith <Surya.dheeshjith@gmail.com>
2023-04-19 19:44:16 -0400

    Minor fix in test

--- check_data_files.py
+++ check_data_files.py
@@ -1,9 +1,9 @@
 import os.path as osp
 
 try:
-    assert True == osp.isdir("dataset/dataset/train")
-    assert True == osp.isdir("dataset/dataset/unlabeled")
-    assert True == osp.isdir("dataset/dataset/val")
+    assert True == osp.isdir("/dataset/dataset/train")
+    assert True == osp.isdir("/dataset/dataset/unlabeled")
+    assert True == osp.isdir("/dataset/dataset/val")
 
 except:
     print("Error in accessing datasets")
